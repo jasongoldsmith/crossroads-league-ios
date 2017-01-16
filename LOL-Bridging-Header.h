5884c04bee61ee8fdfb93fd5c2a93fee676c6a9e
Thakur, Ashutosh Singh <ashutosh.thakur@one.verizon.com>
2017-01-16 12:19:21 -0800

    Added files

---
+++ LOL-Bridging-Header.h
@@ -0,0 +1,19 @@
+//
+//  LOL-Bridging-Header.h
+//  LOL
+//
+//  Created by Ashutosh, Thakur on 1/11/17.
+//  Copyright © 2017 Catalyst Foundry LLC. All rights reserved.
+//
+
+#ifndef LOL_Bridging_Header_h
+#define LOL_Bridging_Header_h
+
+#import <SDWebImage/UIImageView+WebCache.h>
+#import "SDWebImage/UIImageView+WebCache.h"
+#import "TTTAttributedLabel/TTTAttributedLabel.h"
+#import "Mixpanel/Mixpanel.h"
+#import "iCarousel/iCarousel.h"
+
+
+#endif /* LOL_Bridging_Header_h */
--- README.md
+++ README.md
@@ -1,2 +1,1 @@
 # overwatch-ios
- - description goes here!
