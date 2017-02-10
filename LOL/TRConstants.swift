e7d12b6f4cff75e05b9e416c316b333f4f98aab0
Thakur, Ashutosh Singh <ashutosh.thakur@one.verizon.com>
2017-02-10 11:14:14 -0800

    Chnage password UI update and request endpoint update

--- LOL/TRConstants.swift
+++ LOL/TRConstants.swift
@@ -94,7 +94,7 @@ struct K {
         static let TR_GET_GROUPS         =   "/api/v1/a/account/group/list"
         static let TR_UPDATE_GROUPS      =   "/api/v1/a/user/updateGroup"
         static let TR_GET_GROUP_BY_ID    =   "/api/v1/a/account/group/search/"
-        static let TR_UPDATE_PASSWORD    =   "/api/v1/a/user/updatePassword"
+        static let TR_UPDATE_PASSWORD    =   "/api/v1/a/user/changePassword"
         static let TR_RESEND_VERIFICATION =  "/api/v1/a/account/group/resendBungieMessage"
         static let TR_BUNGIE_USER_AUTH   =   "/api/v1/auth/checkBungieAccount"
         static let TR_GROUP_NOTI_VALUE   =   "/api/v1/a/account/group/mute"
