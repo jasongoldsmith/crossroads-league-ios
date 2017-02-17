e8dd2cf15ef0b7ec424cf02392a8838a45d16602
Thakur, Ashutosh Singh <ashutosh.thakur@one.verizon.com>
2017-02-17 14:35:20 -0800

    after user sends an email via Contact Us https://zpl.io/Zd62jM

--- LOL/ViewControllers/TRMessageSentConfViewController.swift
+++ LOL/ViewControllers/TRMessageSentConfViewController.swift
@@ -7,11 +7,32 @@
 //
 
 import Foundation
+import TTTAttributedLabel
+
 
 class TRMessageSentConfViewController: TRBaseViewController {
     
+    
+    @IBOutlet weak var titleLabel: UILabel!
+    @IBOutlet weak var bodyLabel: TTTAttributedLabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        let messageText = "Thank you! Your message has been sent to support@crossroadsapp.co and we will email you if we need additional details."
+        self.titleLabel.text = "MESSAGE SENT"
+        self.bodyLabel.text = messageText
+        self.bodyLabel?.activeLinkAttributes
+        let subscriptionNoticeLinkAttributes = [
+            NSForegroundColorAttributeName: UIColor(red: 198/255, green: 127/255, blue: 6/255, alpha: 1),
+            NSUnderlineStyleAttributeName: NSNumber(bool:true),
+            ]
+        
+        let nsString = messageText as NSString
+        let emailID = "support@crossroadsapp.co"
+        let emailIDRange = nsString.rangeOfString(emailID)
+        self.bodyLabel?.linkAttributes = subscriptionNoticeLinkAttributes
+        self.bodyLabel?.addLinkToURL(NSURL(string:""), withRange: emailIDRange)
     }
     
     override func viewDidAppear(animated: Bool) {
